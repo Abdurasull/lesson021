0497d1d2d61cb65dd06856b1eaec3692d2c37612
Abdurasull <abdurasulsalimov94@gmail.com>
2024-12-14 22:07:27 +0500

    books.c bajarildi

--- books.c
+++ books.c
@@ -1,51 +1,54 @@
 #include<stdio.h>
 
-struct User { //User nomli struct yaraddik
-    char lastname[50];  
-    char firsname[50];
-    char addres[100]; 
-    int birthyear;
-    char phonnumber[20];
+struct Author {
+    //Kitobni Authori uchun struct yaratildi
+    char name[50];
+    int birth_year;
+};
+
+struct Book
+{
+    //Kitob yaratish uchun struct yaratildi
+    char title[50];
+    float price;
+    int published_year;
+    struct Author Author;
 };
 
 int main(){
-    int n;
-    printf("Userlar sonini kiriting: ");
-    scanf("%d", &n);
-    struct User users[n];
 
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d-User ismini kiriting:", i + 1 );
-        scanf("%s", users[i].firsname);
+    struct Author author01 = {"Alex Black", 1990};
+    struct Author author02 = {"Mary White", 1985};
+    struct Author author03 = {"John Green", 1975};
 
-        printf("%d-User familiyasini kiriting:", i + 1);
-        scanf("%s", users[i].lastname);
+    struct Book books[10] = {
+        {"Book A", 25.0, 2021, author01},
+        {"Book B", 30.0, 2023, author02},
+        {"Book C", 15.0, 1997, author03},
+        {"Book D", 20.0, 1880, author02},
+        {"Book E", 35.0, 1994, author02},
+        {"Book F", 40.0, 1995, author01},
+        {"Book G", 10.0, 1790, author02},
+        {"Book H", 50.0, 1890, author01},
+        {"Book I", 45.0, 1990, author02},
+        {"Book J", 60.0, 1890, author03}
+        };
 
-        printf("%d-User manzilini kiriting:", i + 1);
-        scanf("%s", users[i].addres);
-
-        printf("%d-User tugilgan yilini kiriting:", i + 1);
-        scanf("%d", &users[i].birthyear);
-
-        printf("%d-User tel nomirini kiriting:", i + 1);
-        scanf("%s", users[i].phonnumber);
+    printf("XXI asr kitoblari:\n     Title|  Price| Published Year|  Author Name| Author Birth Year\n");
+    printf("------------------------------------------------------------------\n");
+    for (int i = 0; i < 10; i++)
+    {//21 asr kitoblarini chiqarish un for loop dan foydalandik
+        if (books[i].published_year >=2000)
+        {
+            printf("%10s", books[i].title);
+            printf("| %6.1f", books[i].price);
+            printf("| %14d", books[i].published_year);
+            printf("| %12s", books[i].Author.name);
+            printf("| %d\n", books[i].Author.birth_year);
+        }
+        
     }
+    
 
-    printf("Foydalanuvchilar Jadvali\n");
-    printf("First Name | Last Name | Birth Year | Location | Phone\n");
-    printf("----------------------------------------------------------\n");
-
-    for (int i = 0; i < n; i++)
-    {
-        printf("%11s", users[i].firsname);
-
-        printf("| %10s", users[i].lastname);
-
-        printf("| %11d", users[i].birthyear);
-
-        printf("| %9s", users[i].addres);
-
-        printf("| %s\n", users[i].phonnumber);
-    }
+    return 0;
 }
